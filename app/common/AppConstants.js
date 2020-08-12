dd378be655b030c2924b9495ced68164034b90f2
Gavin Orland <gavin.orland@gmail.com>
2020-08-12 11:37:18 +0100

    Sort constants

--- app/common/AppConstants.js
+++ app/common/AppConstants.js
@@ -1,35 +1,35 @@
 export default class AppConstants {
   static APP_NAME = 'Central Operations Platform';
 
+  static CALENDAR_PATH = '/calendar';
+
+  static CASES_PATH = '/cases';
+
   static DASHBOARD_PATH = '/dashboard';
-
-  static YOUR_TASKS_PATH = '/your-tasks';
-
-  static YOUR_GROUP_TASKS_PATH = '/your-group-tasks';
 
   static FORMS_PATH = '/forms';
 
-  static REPORTS_PATH = '/reports';
+  static MESSAGES_PATH = '/messages';
+
+  static MOBILE_WIDTH = 640;
+
+  static ONBOARD_USER_PATH = '/onboard-user';
+
+  static PROCEDURE_DIAGRAM_PATH = '/procedure-diagram';
+
+  static REFRESH_TIMEOUT = 300000;
 
   static REPORT_PATH = '/report';
 
-  static MESSAGES_PATH = '/messages';
+  static REPORTS_PATH = '/reports';
 
-  static CALENDAR_PATH = '/calendar';
+  static SHIFT_PATH = '/shift';
 
   static SUBMIT_A_FORM = '/submit-a-form';
 
   static TASK_PATH = '/task';
 
-  static SHIFT_PATH = '/shift';
+  static YOUR_GROUP_TASKS_PATH = '/your-group-tasks';
 
-  static PROCEDURE_DIAGRAM_PATH = '/procedure-diagram';
-
-  static ONBOARD_USER_PATH = '/onboard-user';
-
-  static CASES_PATH = '/cases';
-
-  static REFRESH_TIMEOUT = 300000;
-
-  static MOBILE_WIDTH = 640;
+  static YOUR_TASKS_PATH = '/your-tasks';
 }
