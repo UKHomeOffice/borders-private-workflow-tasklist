fbd4e53a92dbf490f1c7795702396b6257175ca8
Gavin Orland <gavin.orland@gmail.com>
2020-12-23 15:44:00 +0000

    Ensure cached task data is specific to the user

--- app/pages/task/form/components/TaskForm.jsx
+++ app/pages/task/form/components/TaskForm.jsx
@@ -102,10 +102,10 @@ export default class TaskForm extends React.Component {
       }
 
     }
-    if (secureLocalStorage.get(task.get('id'))) {
+    if (secureLocalStorage.get(`${task.get('id')}::${kc.tokenParsed.email}`)) {
       submission.data = {
         ...submission.data,
-        ...secureLocalStorage.get(task.get('id'))
+        ...secureLocalStorage.get(`${task.get('id')}::${kc.tokenParsed.email}`)
       }
     }
     submission.data = {
@@ -189,7 +189,7 @@ export default class TaskForm extends React.Component {
           }
         }}
         onChange={instance => {
-          secureLocalStorage.set(task.get('id'),
+          secureLocalStorage.set(`${task.get('id')}::${kc.tokenParsed.email}`,
               _.omit(instance.data,
                   ['keycloakContext',
                    'shiftDetailsContext',
